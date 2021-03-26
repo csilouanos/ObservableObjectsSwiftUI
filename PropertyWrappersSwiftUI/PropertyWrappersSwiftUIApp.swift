dbeffd8f2c0c47fd5a2ea45cc92fa5db77d3955f
Silouanos Nicolaou <sni@ttcm.com>
2021-03-26 09:22:39 +0200

    Initial Commit

---
+++ PropertyWrappersSwiftUI/PropertyWrappersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersSwiftUIApp.swift
+//  PropertyWrappersSwiftUI
+//
+//  Created by Silouanos Nicolaou on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
